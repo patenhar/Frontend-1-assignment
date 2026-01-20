b1506f6785109d8d175453d8c8410aa050b841f1
Harsh Patel <harsh.patel@dialogos.fi>
2026-01-20 17:34:49 +0530

    Added tags and uuid for product cards

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -5,6 +5,7 @@ interface ProductCardProps {
   title: string;
   description: string;
   price: number;
+  quantity: number;
 }
 
 export default function ProductCard({
@@ -12,21 +13,34 @@ export default function ProductCard({
   title,
   description,
   price,
+  quantity,
 }: ProductCardProps) {
   return (
     <div className="bg-gray-100 w-70 flex flex-col items-center border-0 rounded-sm">
-      <div className="border-0 rounded-sm">
+      <div className="border-0 rounded-sm relative">
         <img
           src={imageUrl}
           alt="Product image"
           className="border-0 rounded-sm"
         />
+        <div className="absolute top-5 right-5 flex flex-col gap-2">
+          {quantity < 5 && (
+            <div className="p-1 px-2 text-center border-0 rounded-2xl bg-orange-500 text-white">
+              Low stock
+            </div>
+          )}
+          {price > 500 && (
+            <div className="p-1 px-2 text-center border-0 rounded-2xl bg-yellow-500 text-white">
+              Premium
+            </div>
+          )}
+        </div>
       </div>
       <div className="flex flex-col items-start w-full p-5">
         <h1 className="font-bold">{title}</h1>
         <p>{description}</p>
         <h5>$ {price}</h5>
-        <button className="bg-blue-500 text-white border-0 rounded-sm p-2 w-full">
+        <button className="bg-blue-500 text-white border-0 rounded-sm p-2 w-full cursor-pointer">
           Buy Now
         </button>
       </div>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,28 +1,86 @@
 import React from "react";
 import ProductCard from "../Components/ProductCard";
-import image from "../assets/OIP.jpg";
+import image from "../assets/64c590c754d6bc13ebd90cbc_ai_product_photo_styles.jpg";
+import { v4 as uuidv4 } from "uuid";
 
 const products = [
-  { imageUrl: image, title: "Product-1", description: "Product-1", price: 11 },
-  { imageUrl: image, title: "Product-2", description: "Product-2", price: 22 },
-  { imageUrl: image, title: "Product-3", description: "Product-3", price: 33 },
-  { imageUrl: image, title: "Product-4", description: "Product-4", price: 44 },
-  { imageUrl: image, title: "Product-5", description: "Product-5", price: 55 },
-  { imageUrl: image, title: "Product-5", description: "Product-5", price: 55 },
-  { imageUrl: image, title: "Product-5", description: "Product-5", price: 55 },
-  { imageUrl: image, title: "Product-5", description: "Product-5", price: 55 },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-1",
+    description: "Product-1",
+    price: 11,
+    quantity: 1,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-2",
+    description: "Product-2",
+    price: 543,
+    quantity: 2,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-3",
+    description: "Product-3",
+    price: 33,
+    quantity: 3,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-4",
+    description: "Product-4",
+    price: 871,
+    quantity: 4,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-5",
+    description: "Product-5",
+    price: 55,
+    quantity: 5,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-5",
+    description: "Product-5",
+    price: 55,
+    quantity: 6,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-5",
+    description: "Product-5",
+    price: 555,
+    quantity: 7,
+  },
+  {
+    id: uuidv4(),
+    imageUrl: image,
+    title: "Product-5",
+    description: "Product-5",
+    price: 55,
+    quantity: 8,
+  },
 ];
 
 export default function Home() {
   return (
     <div className="p-10 gap-10 flex flex-wrap justify-start">
-      {products.map((product, idx) => (
+      {products.map((product) => (
         <ProductCard
           imageUrl={product.imageUrl}
           title={product.title}
           description={product.description}
           price={product.price}
-          key={idx}
+          quantity={product.quantity}
+          key={product.id}
         />
       ))}
     </div>
